eafa3238864d2a5a2bc34208c99dcc746e463606
WillemBaeyensPXL <willem.baeyens@student.pxl.be>
2026-02-12 14:40:15 +0100

    made text, cyan

--- ClonedProject/ClonedProject/Program.cs
+++ ClonedProject/ClonedProject/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
             Console.WriteLine("Proficiat, je hebt je eerste clone gemaakt!");
+            Console.ResetColor();
         }
     }
 }
